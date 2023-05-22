90d57c5eb58f09e6cd79683d82b044ea2b10a8bc
Stine <stine.jonassen.foyen@vlfk.no>
2023-05-22 11:32:13 +0200

    added media queries

--- resources/js/year.js
+++ resources/js/year.js
@@ -2,3 +2,12 @@ const today = new Date();
 const year = today.getFullYear();
 console.log(year); 
 document.getElementById("year").innerText = year; 
+
+function myFunction() {
+var x = document.getElementById("myTopnav");
+if (x.className === "links") {
+x.className += " responsive";
+} else {
+x.className = "links";
+}
+}
